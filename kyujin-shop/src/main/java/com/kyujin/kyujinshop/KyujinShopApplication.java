1053807e21961934d1284b548a7764642ef529c8
kyujin <kyujin@hagyujin-ui-MacBookPro.local>
2022-06-22 14:18:55 +0900

    study commit

---
+++ kyujin-shop/src/main/java/com/kyujin/kyujinshop/KyujinShopApplication.java
@@ -0,0 +1,13 @@
+package com.kyujin.kyujinshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KyujinShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KyujinShopApplication.class, args);
+	}
+
+}
---
+++ kyujin-shop/src/test/java/com/kyujin/kyujinshop/KyujinShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kyujin.kyujinshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KyujinShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
