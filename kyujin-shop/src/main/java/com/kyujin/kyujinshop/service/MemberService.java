4b2bd4aac34d2ebfcfa6741a8a0b21b64a68f7f3
kyujin <kyujin@hagyujin-ui-MacBookPro.local>
2022-06-23 22:16:36 +0900

    dev memberService

---
+++ kyujin-shop/src/main/java/com/kyujin/kyujinshop/service/MemberService.java
@@ -0,0 +1,44 @@
+package com.kyujin.kyujinshop.service;
+
+import com.kyujin.kyujinshop.domain.Member;
+import com.kyujin.kyujinshop.repository.MemberRepository;
+import com.kyujin.kyujinshop.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    /*
+    * sign up
+    * */
+
+    public Long join(Member member){
+
+        validateDuplilcateMember(member);//중복막기
+
+
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplilcateMember(Member member) {
+        memberRepository.findByName(member.getName())
+             .ifPresent(m -> {
+             throw new IllegalStateException("이미 존재하는 회원입니다.");
+         });
+    }
+    /*
+    * 전체 회원 조회
+    * */
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId){
+        return memberRepository.findById(memberId);
+    }
+
+}
