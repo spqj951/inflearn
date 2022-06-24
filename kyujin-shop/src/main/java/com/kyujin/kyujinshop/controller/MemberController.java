7345c7b7da867975b3032f2ed335077aa07a5cac
kyujin <kyujin@hagyujin-ui-MacBookPro.local>
2022-06-24 10:42:55 +0900

    base finish

--- kyujin-shop/src/main/java/com/kyujin/kyujinshop/controller/MemberController.java
+++ kyujin-shop/src/main/java/com/kyujin/kyujinshop/controller/MemberController.java
@@ -1,15 +1,21 @@
 package com.kyujin.kyujinshop.controller;
 
+import com.kyujin.kyujinshop.domain.Member;
 import com.kyujin.kyujinshop.repository.MemberRepository;
 import com.kyujin.kyujinshop.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
 
-    //field injection
-    @Autowired
+    //field injection, final 빠짐
+    //@Autowired
     private MemberService memberService;
 
     //constructor injection == the best
@@ -20,9 +26,31 @@ public class MemberController {
 
     //setter injection
     //anyone can get this
+    @Autowired
     public void setMemberService(MemberService memberService){
         this.memberService = memberService;
     }
 
+    @GetMapping("/members/new")
+    public String createForm(){
+        return "members/createMemberForm";
+    }
 
+    @PostMapping("/members/new")
+    public String create(MemberForm form){
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model){
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
+
+    }
 }
